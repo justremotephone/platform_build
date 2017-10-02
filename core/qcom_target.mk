70fc344b97e623d762b9a74a552558b9f66b7fac
Thomas Lehner <thomas.lehner@justremotephone.com>
2017-10-02 18:24:03 +0200

    Make our build ready for Android O bringup.

--- core/qcom_target.mk
+++ core/qcom_target.mk
@@ -35,10 +35,10 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
         endif
     endif
 
-    TARGET_GLOBAL_CFLAGS += $(qcom_flags)
-    TARGET_GLOBAL_CPPFLAGS += $(qcom_flags)
-    CLANG_TARGET_GLOBAL_CFLAGS += $(qcom_flags)
-    CLANG_TARGET_GLOBAL_CPPFLAGS += $(qcom_flags)
+    #TARGET_GLOBAL_CFLAGS += $(qcom_flags)
+    #TARGET_GLOBAL_CPPFLAGS += $(qcom_flags)
+    #CLANG_TARGET_GLOBAL_CFLAGS += $(qcom_flags)
+    #CLANG_TARGET_GLOBAL_CPPFLAGS += $(qcom_flags)
 
     # Multiarch needs these too..
     2ND_TARGET_GLOBAL_CFLAGS += $(qcom_flags)
